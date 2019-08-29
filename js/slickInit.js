5840530b69ee0f0dd743ac8653e3dcf142152ca1
blissfulrepair <blissfulrepair@gmail.com>
2019-08-28 22:41:46 -0600

    did I have document in double quotes this whole time?

--- js/slickInit.js
+++ js/slickInit.js
@@ -1,5 +1,5 @@
     <script type="text/javascript">
-      $("document").ready({ function(){
+      $(document).ready({ function(){
         $(".panel.review").slick({
           autoplay: true,
           autoplaySpeed: 3000, 
