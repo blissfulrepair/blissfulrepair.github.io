8b6d51b742cd3b86b11a9a1052eb700d850edadb
blissfulrepair <blissfulrepair@gmail.com>
2019-08-28 22:51:36 -0600

    add dots and delay the slides one second longer

--- js/slickInit.js
+++ js/slickInit.js
@@ -2,7 +2,7 @@
       $(document).ready(function(){
         $(".panel.review").slick({
           autoplay: true,
-          autoplaySpeed: 3000, 
+          autoplaySpeed: 4000, 
           dots: true 
         });
      });
