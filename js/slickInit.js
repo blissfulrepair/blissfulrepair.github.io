3ddcb0c4caa56c00741e747e0e1782350ade814e
blissfulrepair <blissfulrepair@gmail.com>
2019-08-28 22:32:01 -0600

    maybe this will work? moved slick init code to seperate file and added it to js.html the same way other includes appear

---
+++ js/slickInit.js
@@ -0,0 +1,11 @@
+    <script type="text/javascript">
+      window.onload = function(){
+        $(".panel.review").slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000, 
+          dots: true
+        });
+     };
+
